08b26c588a5a94bf003cadf33f810c54b173806e
Aristotelis Sibetheros <50674959+Aristotelis-Sib@users.noreply.github.com>
2023-01-17 14:08:20 +0200

    Create testingPathControl.m

---
+++ testingPathControl.m
@@ -0,0 +1,23 @@
+function tests = testingPathControl(pathList)
+    numOfPaths = length(pathList);
+    tests = zeros(1,numOfPaths);
+    for iPath = 1:numOfPaths
+        tests(iPath) = isPathForModelTest(pathList(iPath));
+    end
+    
+end    
+
+
+
+
+function ForModelTestFlag = isPathForModelTest(path)
+    if contains(path,"Apps") && ~(contains(path,fullfile("Should-Cost App","Functions")) || contains(path,"Electrical Steel Viewer App"))
+        ForModelTestFlag = false;
+    elseif contains(path,"Cluster Framework") && ~(contains(path,"+cmTools") || contains(path,"+mtrx"))
+        ForModelTestFlag = false;
+    elseif contains(path,fullfile("_Development space_","Cloud architect"))
+        ForModelTestFlag = false;
+    else 
+        ForModelTestFlag = true;
+    end 
+end 
