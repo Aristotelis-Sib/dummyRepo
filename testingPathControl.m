0ff691f89bf0c652b5fb2423690160ca21b4ddf0
Aristotelis Sibetheros <50674959+Aristotelis-Sib@users.noreply.github.com>
2023-01-19 12:33:32 +0200

    Update testingPathControl.m

--- testingPathControl.m
+++ testingPathControl.m
@@ -1,16 +1,17 @@
-function tests = getTestsToPerform()
-    lines = readlines("changed_files.txt");
-    for iLine = 1:length(lines)
-       disp(lines(iLine))
+function tests = testingPathControl()
+    pathList = "Apps";
+    numOfPaths = length(pathList);
+    temp = zeros(1,numOfPaths);
+    for iPath = 1:numOfPaths
+        temp(iPath) = isPathForModelTest(pathList(iPath));
     end
-    tests =1;
-%     pathList = "Apps";
-%     numOfPaths = length(pathList);
-%     temp = zeros(1,numOfPaths);
-%     for iPath = 1:numOfPaths
-%         temp(iPath) = isPathForModelTest(pathList(iPath));
-%     end
-%     tests = all(temp);
+    
+    if temp
+        tests = "A case";
+    else
+        tests = "B case";
+    end
+    
 end    
 
 function ForModelTestFlag = isPathForModelTest(path)
@@ -24,33 +25,3 @@ function ForModelTestFlag = isPathForModelTest(path)
         ForModelTestFlag = true;
     end 
 end 
-
-% 
-% function tests = testingPathControl()
-%     pathList = "Apps";
-%     numOfPaths = length(pathList);
-%     temp = zeros(1,numOfPaths);
-%     for iPath = 1:numOfPaths
-%         temp(iPath) = isPathForModelTest(pathList(iPath));
-%     end
-%     
-%     if temp
-%         tests = "A case";
-%     else
-%         tests = "B case";
-%     end
-%     
-% end    
-% 
-% function ForModelTestFlag = isPathForModelTest(path)
-%     if contains(path,"Apps") && ~(contains(path,fullfile("Should-Cost App","Functions")) || contains(path,"Electrical Steel Viewer App"))
-%         ForModelTestFlag = false;
-%     elseif contains(path,"Cluster Framework") && ~(contains(path,"+cmTools") || contains(path,"+mtrx"))
-%         ForModelTestFlag = false;
-%     elseif contains(path,fullfile("_Development space_","Cloud architect"))
-%         ForModelTestFlag = false;
-%     else 
-%         ForModelTestFlag = true;
-%     end 
-% end 
-
