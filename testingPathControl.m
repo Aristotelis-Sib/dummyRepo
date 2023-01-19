65d1c2b3e2bdea781f54d70b825820536df8a811
Aristotelis Sibetheros <50674959+Aristotelis-Sib@users.noreply.github.com>
2023-01-19 12:15:06 +0200

    Update testingPathControl.m

--- testingPathControl.m
+++ testingPathControl.m
@@ -1,17 +1,13 @@
-function tests = testingPathControl()
-    pathList = "Apps";
-    numOfPaths = length(pathList);
-    temp = zeros(1,numOfPaths);
-    for iPath = 1:numOfPaths
-        temp(iPath) = isPathForModelTest(pathList(iPath));
-    end
-    
-    if temp
-        tests = "A case";
-    else
-        tests = "B case";
-    end
-    
+function tests = getTestsToPerform()
+    data = importdata('changed_files.txt') ;  
+    disp(data)
+%     pathList = "Apps";
+%     numOfPaths = length(pathList);
+%     temp = zeros(1,numOfPaths);
+%     for iPath = 1:numOfPaths
+%         temp(iPath) = isPathForModelTest(pathList(iPath));
+%     end
+%     tests = all(temp);
 end    
 
 function ForModelTestFlag = isPathForModelTest(path)
@@ -25,3 +21,33 @@ function ForModelTestFlag = isPathForModelTest(path)
         ForModelTestFlag = true;
     end 
 end 
+
+% 
+% function tests = testingPathControl()
+%     pathList = "Apps";
+%     numOfPaths = length(pathList);
+%     temp = zeros(1,numOfPaths);
+%     for iPath = 1:numOfPaths
+%         temp(iPath) = isPathForModelTest(pathList(iPath));
+%     end
+%     
+%     if temp
+%         tests = "A case";
+%     else
+%         tests = "B case";
+%     end
+%     
+% end    
+% 
+% function ForModelTestFlag = isPathForModelTest(path)
+%     if contains(path,"Apps") && ~(contains(path,fullfile("Should-Cost App","Functions")) || contains(path,"Electrical Steel Viewer App"))
+%         ForModelTestFlag = false;
+%     elseif contains(path,"Cluster Framework") && ~(contains(path,"+cmTools") || contains(path,"+mtrx"))
+%         ForModelTestFlag = false;
+%     elseif contains(path,fullfile("_Development space_","Cloud architect"))
+%         ForModelTestFlag = false;
+%     else 
+%         ForModelTestFlag = true;
+%     end 
+% end 
+
